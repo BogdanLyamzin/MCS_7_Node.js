fa16fcb866940db0456a153bc3a835b3bc368002
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-11-12 19:27:07 +0100

    move database config to config.js

--- lesson-5/project-backend/db/sequelize.js
+++ lesson-5/project-backend/db/sequelize.js
@@ -1,15 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-    dialect: "postgres",
-    username: "my_movies_v3h7_user",
-    password: "5IWjdbG4cct0yNX3r2GenLoEepoZadck",
-    host: "dpg-d4acgoggjchc73fke980-a.frankfurt-postgres.render.com",
-    database: "my_movies_v3h7",
-    port: 5432,
-    dialectOptions: {
-        ssl: true
-    }
-});
+import { databaseConfig } from "../config.js";
+
+const sequelize = new Sequelize(databaseConfig);
 
 export default sequelize;
